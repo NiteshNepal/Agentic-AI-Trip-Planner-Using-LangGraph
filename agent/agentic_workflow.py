585b48ee5f4a9e3eb1f36cbd40e078223122e663
NiteshNepal <niteshnepal101@gmail.com>
2025-07-08 23:25:39 +0530

    end point added

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -27,9 +27,7 @@ class GraphBuilder():
         input_question = [self.system_prompt] + user_question
         response = self.llm_with_tools.invoke(input_question)
         return {"messages": [response]}
-    
-    def build_function(self):
-        pass
+
     
     def build_graph(self):
         graph_builder=StateGraph(MessagesState)
